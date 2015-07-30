4567ab03ab09d62144974b16da064856f8397472
Rafael nuñez <1995nunezrafael@gmail.com>
2015-07-30 16:57:36 -0500

    Layout  Registrate creado

--- app/src/main/java/com/example/rn_dr/innovationanpr/MainActivity.java
+++ app/src/main/java/com/example/rn_dr/innovationanpr/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.rn_dr.innovationanpr;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,10 +21,20 @@ public class MainActivity extends AppCompatActivity {
     SlidingTabLayout tabs;
     CharSequence Titles[]={"Iniciar sesion","Registrate"};
     int Numboftabs =2;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*sharedPreferences=getSharedPreferences("VALUES",MODE_PRIVATE);
+        int theme=sharedPreferences.getInt("THEME",1);
+        switch (theme){
+            case 1: setTheme(R.style.AppTheme);
+                break;
+            case 2: setTheme(R.style.AppTheme2);
+
+                break;
+        }*/
         setContentView(R.layout.activity_main);
 
 
@@ -41,42 +54,48 @@ public class MainActivity extends AppCompatActivity {
         // Assiging the Sliding Tab Layout View
         tabs = (SlidingTabLayout) findViewById(R.id.tabs);
         tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
-
-        // Setting Custom Color for the Scroll bar indicator of the Tab View
-        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+        /*tabs.setOnClickListener(new View.OnClickListener() {
             @Override
-            public int getIndicatorColor(int position) {
-                return getResources().getColor(R.color.tabsScrollColor);
+            public void onClick(View v) {
+                sharedPreferences.edit().putInt("THEME", 1).apply();
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+
             }
-        });
+        });*/
+                // Setting Custom Color for the Scroll bar indicator of the Tab View
+                tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+                    @Override
+                    public int getIndicatorColor(int position) {
+                        return getResources().getColor(R.color.tabsScrollColor);
+                    }
+                });
 
-        // Setting the ViewPager For the SlidingTabsLayout
-        tabs.setViewPager(pager);
+                // Setting the ViewPager For the SlidingTabsLayout
+                tabs.setViewPager(pager);
+
+            }
 
 
+            @Override
+            public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.menu_main, menu);
+                return true;
+            }
 
-    }
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                // Handle action bar item clicks here. The action bar will
+                // automatically handle clicks on the Home/Up button, so long
+                // as you specify a parent activity in AndroidManifest.xml.
+                int id = item.getItemId();
 
+                //noinspection SimplifiableIfStatement
+                if (id == R.id.action_settings) {
+                    return true;
+                }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                return super.onOptionsItemSelected(item);
+            }
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
