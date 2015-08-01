d6d3bf7c369270fa687b24637afa63f52e2a587e
Rafael nuñez <1995nunezrafael@gmail.com>
2015-08-01 01:10:18 -0500

    Se llama al nuevo layout de configuración

--- app/src/main/java/com/example/rn_dr/innovationanpr/PreConfigEntity.java
+++ app/src/main/java/com/example/rn_dr/innovationanpr/PreConfigEntity.java
@@ -1,98 +1,16 @@
 package com.example.rn_dr.innovationanpr;
 
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
-/**
- * Created by RN_DR on 30/07/2015.
- */
-public class PreConfigEntity extends AppCompatActivity {
-    Toolbar toolbar;
-    ViewPager pager;
-    ViewPagerAdapter adapter;
-    SlidingTabLayout tabs;
-    CharSequence Titles[]={"Iniciar sesion","Registrate"};
-    int Numboftabs =2;
-    SharedPreferences sharedPreferences;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class PreConfigEntity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*sharedPreferences=getSharedPreferences("VALUES",MODE_PRIVATE);
-        int theme=sharedPreferences.getInt("THEME",1);
-        switch (theme){
-            case 1: setTheme(R.style.AppTheme);
-                break;
-            case 2: setTheme(R.style.AppTheme2);
-
-                break;
-        }*/
-        setContentView(R.layout.activity_main);
-
-
-        // Creating The Toolbar and setting it as the Toolbar for the activity
-
-        toolbar = (Toolbar) findViewById(R.id.tool_bar);
-        setSupportActionBar(toolbar);
-
-
-        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
-        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
-
-        // Assigning ViewPager View and setting the adapter
-        pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(adapter);
-
-        // Assiging the Sliding Tab Layout View
-        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
-        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
-        /*tabs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sharedPreferences.edit().putInt("THEME", 1).apply();
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(intent);
-
-            }
-        });*/
-        // Setting Custom Color for the Scroll bar indicator of the Tab View
-        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
-            @Override
-            public int getIndicatorColor(int position) {
-                return getResources().getColor(R.color.tabsScrollColor);
-            }
-        });
-
-        // Setting the ViewPager For the SlidingTabsLayout
-        tabs.setViewPager(pager);
-
+        setContentView(R.layout.regis_entity_2_layout);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/example/rn_dr/innovationanpr/Tab2.java
+++ app/src/main/java/com/example/rn_dr/innovationanpr/Tab2.java
@@ -1,19 +1,31 @@
 package com.example.rn_dr.innovationanpr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.example.rn_dr.innovationanpr.R;
+import android.widget.Button;
 
 
 public class Tab2 extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.regis_entity_layout,container,false);
+    public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.regis_entity_layout, container, false);
+        Button btnsiguiente = (Button) v.findViewById(R.id.btnSiguiente);
+        btnsiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), PreConfigEntity.class);
+                startActivity(intent);
+            }
+        });
         return v;
     }
 }
+
+
+
