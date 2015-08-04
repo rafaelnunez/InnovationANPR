4561ad17c9e0550ae9660f316ef32efbb001633c
Rafael nuñez <1995nunezrafael@gmail.com>
2015-08-04 10:09:28 -0500

    Error Tab2 corregido

--- app/src/main/java/com/example/rn_dr/innovationanpr/PreConfigEntity.java
+++ app/src/main/java/com/example/rn_dr/innovationanpr/PreConfigEntity.java
@@ -3,12 +3,14 @@ package com.example.rn_dr.innovationanpr;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 
-public class PreConfigEntity extends Activity {
+public class PreConfigEntity extends AppCompatActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.regis_entity_2_layout);
     }
--- app/src/main/java/com/example/rn_dr/innovationanpr/Tab2.java
+++ app/src/main/java/com/example/rn_dr/innovationanpr/Tab2.java
@@ -11,12 +11,12 @@ import android.widget.Button;
 
 
 public class Tab2 extends Fragment {
-
+    private Button btnsiguie;
     @Override
     public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.regis_entity_layout, container, false);
-        Button btnsiguiente = (Button) v.findViewById(R.id.btnSiguiente);
-        btnsiguiente.setOnClickListener(new View.OnClickListener() {
+        btnsiguie = (Button) v.findViewById(R.id.btnSigui);
+        btnsiguie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), PreConfigEntity.class);
