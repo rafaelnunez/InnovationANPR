cfeb7bfd4daa866f29624a39f2855d67b7bafa56
jmesquivel19 <j.esquivel_12@hotmail.com>
2015-08-04 10:11:54 -0500

    Inserté animaciones para la transición entre activitys Creé dos layout el de tipo de entidad y el de registrar datos de entidad, estan apartes o sea que no estan interconectados. Si se quiere probar, después del splash no se llama al MainActiviity si no que se llama a cada uno de los layouts

--- app/src/main/java/com/example/rn_dr/innovationanpr/registra_parqueadero.java
+++ app/src/main/java/com/example/rn_dr/innovationanpr/registra_parqueadero.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
 
 
 public class registra_parqueadero extends ActionBarActivity {
@@ -11,6 +12,7 @@ public class registra_parqueadero extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Spinner tipo_vehiculo = (Spinner) findViewById(R.id.SpTipoVehiculo);
         setContentView(R.layout.activity_registra_parqueadero);
     }
 
