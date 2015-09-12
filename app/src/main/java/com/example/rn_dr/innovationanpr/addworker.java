a83a380ff0f418c9ebceae6ebaa9ae54e45e9fcf
jmesquivel19 <j.esquivel_12@hotmail.com>
2015-09-12 17:12:51 -0500

    Muchas modificaciones De Admin

---
+++ app/src/main/java/com/example/rn_dr/innovationanpr/addworker.java
@@ -0,0 +1,41 @@
+package com.example.rn_dr.innovationanpr;
+
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class addworker extends DialogFragment {
+
+
+    public addworker() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       
+        return inflater.inflate(R.layout.fragment_addworker, container, false);
+    }
+    /*  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_edit_name, container);
+        mEditText = (EditText) view.findViewById(R.id.txt_your_name);
+        getDialog().setTitle("Hello");
+
+        return view;
+    }
+  */
+
+}
